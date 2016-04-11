090685e8ad6bf93bdfc3ef80ab123708274d4eb0
Roland Vachter <roland.vachter@iquestgroup.com>
2016-04-11 16:33:11 +0100

    Embed brightcove videos using n-video.

--- src/js/ui/embeddedMedia.js
+++ src/js/ui/embeddedMedia.js
@@ -1,5 +1,7 @@
 /* global BrightcoveFT, brightcove */
 
+const nVideo = require('n-video');
+
 const domUtils = require('../utils/dom');
 const base64 = require('../utils/base64');
 
@@ -31,32 +33,16 @@ function convertTweet (embeddedTweetElement) {
 
 function convertFTVideo (embeddedFtVideoElement) {
 	return new Promise((resolve) => {
-		let width;
-		let height;
 		let videoId;
 		let replacementHtml;
 
 		embeddedFtVideoElement.setAttribute("data-converted", 1);
 
-		// Extract width, height, videoID from element classes
-		width = embeddedFtVideoElement.className.match(/ftvideowidth([0-9]+)/)[1];
-		height = embeddedFtVideoElement.className.match(/ftvideoheight([0-9]+)/)[1];
 		videoId = embeddedFtVideoElement.className.match(/ftvideovideoid([0-9]+)/)[1];
 
-		// Build HTML to embed
-		replacementHtml = "";
-		replacementHtml += "<object width='"+width+"' height='"+height+"'>";
-		replacementHtml += "<param name='wmode' value='transparent' />";
-		replacementHtml += "<param name='movie' value='http://c.brightcove.com/services/viewer/federated_f9?isVid=1&isUI=1' />";
-		replacementHtml += "<param name='flashVars' value='wmode=transparent&@videoPlayer="+videoId+"&playerID=590314128001&playerKey=AQ%2E%2E,AAAACxbljZk%2E,eD0zYozylZ3KmYvlyzd8myNVJz2Gttzx&domain=embed&dynamicStreaming=true' />";
-		replacementHtml += "<param name='base' value='http://admin.brightcove.com' />";
-		replacementHtml += "<param name='seamlesstabbing' value='false' />";
-		replacementHtml += "<param name='allowFullScreen' value='true' />";
-		replacementHtml += "<param name='swLiveConnect' value='true' />";
-		replacementHtml += "<param name='allowScriptAccess' value='always' />";
-		replacementHtml += "<embed src='http://c.brightcove.com/services/viewer/federated_f9?isVid=1&isUI=1' flashVars='wmode=transparent&@videoPlayer="+videoId+"&playerID=590314128001&playerKey=AQ%2E%2E,AAAACxbljZk%2E,eD0zYozylZ3KmYvlyzd8myNVJz2Gttzx&domain=embed&dynamicStreaming=true' base='http://admin.brightcove.com' width='"+width+"' height='"+height+"' seamlesstabbing='false' type='application/x-shockwave-flash' allowFullScreen='true' allowScriptAccess='always' swLiveConnect='true' wmode='transparent'>"
-		replacementHtml += "</embed>";
-		replacementHtml += "</object>";
+		replacementHtml = `<div data-n-component="n-video"
+			data-n-video-source="brightcove"
+			data-n-video-id="${videoId}"></div>`;
 
 		embeddedFtVideoElement.innerHTML = replacementHtml;
 
@@ -87,26 +73,6 @@ function convertYoutubeVideo (embeddedYoutubeVideoElement) {
 	});
 }
 
-function convertBrightcoveVideo (brightcoveEmbed) {
-	return new Promise((resolve) => {
-		// Brightcove is a CDN that takes care of all the videos at videos.ft.com.
-		// To get videos to load, you need to call their 'createExperience' function.
-		// That function is located in a js file that's in the FT Wrapper code.
-		if (typeof BrightcoveFT !== "undefined" && typeof BrightcoveFT.Init !== "undefined" && typeof BrightcoveFT.Init.createExperience !== "undefined") {
-			// Once BrightcoveFT.Init.createExperience is executed, the
-			// brightcove object becomes available. As additional videos are
-			// initialised, they're added to brightcove.experiences.
-
-			brightcoveEmbed.setAttribute("data-converted", 1);
-			if (typeof brightcove === "undefined" || typeof brightcove.experiences === "undefined" || typeof brightcove.experiences[brightcoveEmbed.id] === "undefined") {
-				BrightcoveFT.Init.createExperience(brightcoveEmbed.id);
-			}
-		}
-
-		setTimeout(resolve, 100);
-	});
-}
-
 
 function convertEmbeds(container, selector, fn) {
 	const allEmbeds = container.querySelectorAll(selector);
@@ -131,9 +97,16 @@ function convertEmbeddedVideos(container) {
 	return Promise.all([
 		convertEmbeds(container, "p.embeddedtweet", convertTweet),
 		convertEmbeds(container, "p.embeddedftvideo", convertFTVideo),
-		convertEmbeds(container, "p.embeddedyoutubevideo", convertYoutubeVideo),
-		convertEmbeds(container, ".video-container-ftvideo .BrightcoveExperience", convertBrightcoveVideo)
-	]);
+		convertEmbeds(container, "p.embeddedyoutubevideo", convertYoutubeVideo)
+	]).then(() => {
+		const opts = {
+			optimumWidth: 600,
+			placeholder: true,
+			classes: [],
+			selector: '.video-container-ftvideo .BrightcoveExperience'
+		};
+		nVideo.init(opts);
+	});
 }
 
 exports.convert = convertEmbeddedVideos;
