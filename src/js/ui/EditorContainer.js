54564b3e9d4a34d0e6d05f65ba12edc965486033
Roland Vachter <roland.vachter@iquestgroup.com>
2018-03-19 18:36:09 +0200

    Fix missing reference to FormOverlay.

--- src/js/ui/EditorContainer.js
+++ src/js/ui/EditorContainer.js
@@ -2,6 +2,7 @@ const Delegate = require('dom-delegate');
 const domUtils = require('../utils/dom');
 const templates = require('./templates');
 const AlertOverlay = require('./AlertOverlay');
+const FormOverlay = require('./FormOverlay');
 
 function EditorContainer (webchat, actions) {
 	const self = this;
